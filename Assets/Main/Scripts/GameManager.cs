31d75d6e93f1dbd79346bb90a8702e7eb0b0c7eb
Norbert Sollosi <norbert.sollosi@student.upt.ro>
2024-04-05 22:52:13 +0300

    Added states to GameManager

--- Assets/Main/Scripts/GameManager.cs
+++ Assets/Main/Scripts/GameManager.cs
@@ -8,6 +8,20 @@ namespace Main.Scripts
         public static GameManager Instance;
         // Start is called before the first frame update
 
+        public enum GameState
+        {
+            StartMenu,
+            PlayStarted,
+            Player1Pending,
+            Player1ExecutingAction,
+            Player2Pending,
+            Player2ExecutingAction,
+            RoundEnded,
+            GameFinished
+        }
+
+        private GameState _currentGameState;
+
         private void Awake()
         {
             if (Instance == null)
@@ -22,11 +36,36 @@ namespace Main.Scripts
 
         void Start()
         {
+            _currentGameState = GameState.StartMenu;
         }
 
         // Update is called once per frame
         void Update()
         {
+            switch (_currentGameState)
+            {
+                case GameState.StartMenu:
+                    break;
+                case GameState.PlayStarted:
+                    break;
+                case GameState.Player1Pending:
+                    break;
+                case GameState.Player1ExecutingAction:
+                    break;
+                case GameState.Player2Pending:
+                    break;
+                case GameState.Player2ExecutingAction:
+                    break;
+                case GameState.RoundEnded:
+                    break;
+                case GameState.GameFinished:
+                    break;
+            }
+        }
+
+        public void ChangeState(GameState newState)
+        {
+            _currentGameState = newState;
         }
     }
 }
